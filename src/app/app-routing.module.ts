d40fd62112198d7caf5c210b73b30be7b2100393
Tiago Barbosa <tiagobarbosa02@outlook.com>
2022-06-24 00:22:53 -0300

    Iniciando o projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: '/home', pathMatch: 'ful'},
+  { path: 'home', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
