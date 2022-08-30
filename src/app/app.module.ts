67564b8768d88313a89e008c34ed40605918b67c
Tiago Barbosa <tiagobarbosa02@outlook.com>
2022-08-30 10:54:14 -0300

    Import swiper module e create sermons module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/home/home.component';
@@ -10,6 +9,8 @@ import { FooterComponent } from './components/footer/footer.component';
 import { WhereAreWeComponent } from './modules/where-are-we/where-are-we.component';
 import { ScrollDirective } from './components/navbar/directive/scroll.directive';
 import { AboutModule } from './modules/about/about.module';
+import { SermonsComponent } from './modules/sermons/sermons.component';
+import { SwiperModule } from 'swiper/angular';
 
 @NgModule({
   declarations: [
@@ -18,12 +19,14 @@ import { AboutModule } from './modules/about/about.module';
     NavbarComponent,
     FooterComponent,
     WhereAreWeComponent,
-    ScrollDirective
+    ScrollDirective,
+    SermonsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    SwiperModule,
     AboutModule,
   ],
   providers: [],
---
+++ src/app/modules/sermons/sermons.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import SwiperCore, { Pagination, Navigation } from "swiper";
+SwiperCore.use([Pagination, Navigation]);
+
+@Component({
+  selector: 'app-sermons',
+  templateUrl: './sermons.component.html',
+  styleUrls: ['./sermons.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class SermonsComponent implements OnInit {
+  carouselVideos = [
+    {
+      banner: [
+        {img: '../../../assets/img/background_1.jpg', title: 'It’s Your Door', speaker: 'Pastor Steven Furtick'},
+        {img: '../../../assets/img/background_1.jpg', title: 'It Was A Good Fight', speaker: 'Pastor Larry Bre'},
+        {img: '../../../assets/img/background_1.jpg', title: 'Faithful With A Few', speaker: 'Pastor Steven Furtick'},
+      ],
+    },
+    {
+      banner: [
+        {img: '../../../assets/img/background_2.jpg', title: 'It’s Your Door', speaker: 'Pastor Steven Furtick'},
+        {img: '../../../assets/img/background_2.jpg', title: 'It Was A Good Fight', speaker: 'Pastor Larry Bre'},
+        {img: '../../../assets/img/background_2.jpg', title: 'Faithful With A Few', speaker: 'Pastor Steven Furtick'},
+      ],
+    },
+    {
+      banner: [
+        {img: '../../../assets/img/background_3.jpg', title: 'It’s Your Door', speaker: 'Pastor Steven Furtick'},
+        {img: '../../../assets/img/background_3.jpg', title: 'It Was A Good Fight', speaker: 'Pastor Larry Bre'},
+        {img: '../../../assets/img/background_3.jpg', title: 'Faithful With A Few', speaker: 'Pastor Steven Furtick'},
+      ],
+    },
+    {
+      banner: [
+        {img: '../../../assets/img/background_4.jpg', title: 'It’s Your Door', speaker: 'Pastor Steven Furtick'},
+        {img: '../../../assets/img/background_4.jpg', title: 'It Was A Good Fight', speaker: 'Pastor Larry Bre'},
+        {img: '../../../assets/img/background_4.jpg', title: 'Faithful With A Few', speaker: 'Pastor Steven Furtick'},
+      ],
+    }
+  ]
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
