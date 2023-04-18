ecc6c216fafb35df3cb676b917ef922f3d7bdf03
tiago002pro <tiagobarbosa02@outlook.com>
2023-04-18 06:23:36 -0300

    Novo modulo about

--- src/app/modules/contact/contact.component.ts
+++ src/app/modules/contact/contact.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import SwiperCore, { Autoplay, FreeMode, Navigation, Pagination, Scrollbar, Swiper, SwiperOptions, Thumbs } from "swiper";
+import { SermonService } from '../sermons/service/sermon.service';
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, Autoplay, Thumbs, FreeMode]);
 
 @Component({
   selector: 'app-contact',
@@ -7,8 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
   imgTitle =  './../../../assets/img/Contact.jpg'
+  swiperConfig!: SwiperOptions
+  images!: any
 
-  constructor() { }
+  constructor(
+    private sermonService:SermonService,
+  ) { }
 
-  ngOnInit(): void { }
+  async ngOnInit():Promise<void> {
+    this.swiperConfig = {
+      slidesPerView: 1,
+      navigation: true,
+      scrollbar: { draggable: true },
+      autoplay: {delay: 2500, disableOnInteraction: false},
+    }
+
+    await this.__loadImages()
+  }
+
+  async __loadImages():Promise<void> {
+    this.images = await this.sermonService.getAll().toPromise().then((res) => res)
+  }
 }
