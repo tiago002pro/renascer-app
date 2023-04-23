f6dcc2d45f91311fbd1e8b596017d47a0fb21a63
tiago002pro <tiagobarbosa02@outlook.com>
2023-04-22 23:37:25 -0300

    Melhorias no modulo about

--- src/app/modules/about/about.component.ts
+++ src/app/modules/about/about.component.ts
@@ -1,40 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
+import SwiperCore, { Autoplay, FreeMode, Navigation, Pagination, Scrollbar, Swiper, SwiperOptions, Thumbs } from "swiper";
 
+// SwiperCore.use([Navigation, Pagination, Scrollbar, Autoplay, Thumbs, FreeMode]);
+SwiperCore.use([Navigation, Pagination, Scrollbar]);
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-  imgTitle =  './../../../assets/img/about.jpg'
+  imgTitle =  './../../../assets/img/background_4.jpg'
   slides!: any[]
   visionDescription!: String
   missionDescription!: String
+  swiperConfig!: SwiperOptions
   
   constructor() {}
-  customOptions: OwlOptions = {
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: true,
-    dots: true,
-    navSpeed: 700,
-    autoWidth : true , 
-    navText: ['<<', '>>'],
-    responsive: {
-      0: {
-        items: 1
-      },
-    },
-    nav: false
-  }
   
   ngOnInit(): void {
     this.missionDescription = "Nossa missão é adorar a Deus, proclamar o Evangelho no poder do Espírito Santo, fazer discípulos de Jesus e amar ao próximo."
     this.visionDescription = "Ser uma igreja fiel às Escrituras Sagradas, relevante à sociedade, que promove a transformação do ser humano e o seu desenvolvimento por meio do evangelho, em conformidade com os valores bíblicos."
+    
+    this.swiperConfig = {
+      slidesPerView: 1,
+      navigation: true,
+      pagination: { clickable: true },
+      scrollbar: { draggable: true },
+    }
+
+    
     this.slides = [
       {id: 1, src: './../../../assets/img/background_1.jpg', title: 'Autoridade bíblica', description: 'A Bíblia como fonte de autoridade única de doutrina e conduta prática.'},
       {id: 2, src: './../../../assets/img/background_2.jpg', title: 'Santidade', description: 'A busca da santidade como ideal de vida cristã.'},
