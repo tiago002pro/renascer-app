b5acceadd4b7e970fd78ba54d5a25390fb19fe55
tiago002pro <tiagobarbosa02@outlook.com>
2023-05-25 02:01:31 -0300

    Correções

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -18,9 +18,7 @@ export class FooterComponent implements OnInit {
     this.socialMedia = [
       {name: "Instagran", link: "https://www.instagram.com/renascermaringa/?hl=pt-br", icon: "bi bi-instagram"},
       {name: "Facebook", link: "https://www.facebook.com/igrejarenascermaringa", icon: "bi bi-facebook"},
-      {name: "Twitter", link: "https://twitter.com/renascermaringa", icon: "bi bi-twitter"},
       {name: "Youtube", link: "https://www.youtube.com/c/RenascerMaring%C3%A1", icon: "bi bi-youtube"},
-      {name: "WhatsApp", link: "http://api.whatsapp.com/send?1=pt_BR&phone=554430302570", icon: "bi bi-whatsapp"},
     ]
   }
 
